497c673f8c37d55bb7bb81f6d9d147a46f4a6236
Harris <harris94@vdi-scclab-062.lancs.local>
2023-11-10 13:33:46 +0000

    Updated BombSquare.java - previously there were issues with recursion. Now works as intended

--- BombSquare.java
+++ BombSquare.java
@@ -1,85 +1,86 @@
 import java.util.*;
 
-public class BombSquare extends GameSquare
-{
-	private boolean thisSquareHasBomb = false;
-	public static final int MINE_PROBABILITY = 10;
+/**
+ * Represents a square which might contain a bomb
+ */
+public class BombSquare extends GameSquare {
 
-	public BombSquare(int x, int y, GameBoard board)
-	{
-		super(x, y, "images/blank.png", board);
+    private boolean thisSquareHasBomb = false;
+    private boolean revealed = false;
+    public static final int MINE_PROBABILITY = 10;
 
-		Random r = new Random();
-		thisSquareHasBomb = (r.nextInt(MINE_PROBABILITY) == 0);
-	}
+    /**
+     * Constructor - randomly populates squares with bombs
+     *
+     * @param x     The x-coordinate of the square.
+     * @param y     The y-coordinate of the square.
+     * @param board The game board to which the square belongs.
+     */
+    public BombSquare(int x, int y, GameBoard board) {
+        super(x, y, "images/blank.png", board);
 
-	public void clicked()
-	{
-		if(thisSquareHasBomb) // Square contains a bomb
-		{
-			System.out.println("bomb");
-			setImage("images/bomb.png");
-		} else {
-			if(countBombs(xLocation, yLocation) != 0) /* At least 1 bomb in adjacent squares */ { 
-				System.out.println("bomb adjacent");
-				// Check for bombs in surrounding squares
-				String numImage = "images/" + countBombs(xLocation, yLocation) + ".png";
-				board.getSquareAt(xLocation, yLocation).setImage(numImage);
-			} else /* Empty */{
-				System.out.println("empty");
-				board.getSquareAt(xLocation, yLocation).setImage("images/0.png");
-				revealSurroundingSquares();
-			}
-		}
-	}
+        Random r = new Random();
+        thisSquareHasBomb = (r.nextInt(MINE_PROBABILITY) == 0);
+    }
 
+    /**
+     * Executes if square clicked
+     */
+    public void clicked() {
+        if (thisSquareHasBomb) { // Square contains a bomb
+            setImage("images/bomb.png");
+        } else {
+            if (countSurroundingBombs(xLocation, yLocation) != 0) { // At least 1 surrounding square contains a bomb
+                String numImage = "images/" + countSurroundingBombs(xLocation, yLocation) + ".png";
+                setImage(numImage);
+            } else { // No surrounding squares contain a bomb
+                setImage("images/0.png");
+                revealSurroundingSquares();
+            }
+        }
+    }
 
-	public int countBombs(int x, int y) {
-		int surroundingBombs = 0;
-		for(int j = -1; j <= 1; j++) {
-			for(int i = -1; i <= 1; i++) {
-				try{
-					BombSquare neighbourSquare = (BombSquare)board.getSquareAt(x + i, y + j);
-					if(neighbourSquare.thisSquareHasBomb)
-						surroundingBombs++;
-				} catch(Exception e) {}
-			}
-		}
-		return surroundingBombs;
-	}
+    /**
+     * Counts the number of bombs in the surrounding squares
+     *
+     * @param x The x-coordinate of the square.
+     * @param y The y-coordinate of the square.
+     * @return The number of bombs in the surrounding squares.
+     */
+    public int countSurroundingBombs(int x, int y) {
+        int surroundingBombs = 0;
+        for (int j = -1; j <= 1; j++) {
+            for (int i = -1; i <= 1; i++) {
+                if (board.getSquareAt(x + i, y + j) != null) { // If square doesn't exist .getSquareAt() will return null
+                    BombSquare neighbourSquare = (BombSquare) board.getSquareAt(x + i, y + j);
+                    if (neighbourSquare.thisSquareHasBomb) {
+                        surroundingBombs++;
+                    }
+                }
+            }
+        }
+        return surroundingBombs;
+    }
 
-	public void revealSurroundingSquares() {
-		boolean[][] surrounding = new boolean[3][3]; // true if surrounding bombs == 0
-		for(int j = -1; j <= 1; j++) {
-			for(int i = -1; i <= 1; i++) {
-				if(i == 0 && j == 0)
-					continue;
-				try {
-					int newX = xLocation + i;
-					int newY = yLocation + j;
-					int bombCount = countBombs(newX, newY);
-					String numImage = "images/" + bombCount + ".png";
-					board.getSquareAt(newX, newY).setImage(numImage);
-					if(bombCount == 0)
-						surrounding[i+1][j+1] = true;
-				} catch(Exception e) {};
-			}
-		}
-
-
-		for(int j = 0; j < 3; j++) {
-			for (int i = 0; i < 3; i++) {
-				if (surrounding[i][j]) {
-					int newX = xLocation - 1 + i;
-					int newY = yLocation - 1 + j;
-
-					try {
-						BombSquare tempBoard = (BombSquare) board.getSquareAt(newX, newY);
-						tempBoard.revealSurroundingSquares();
-					} catch(Exception e) {}
+    /**
+     * Reveals the surrounding squares of the current square
+     */
+    public void revealSurroundingSquares() {
+        revealed = true;
+        for (int j = -1; j <= 1; j++) {
+            for (int i = -1; i <= 1; i++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+				int nextX = xLocation + i;
+				int nextY = yLocation + j;
+				if (board.getSquareAt(nextX, nextY) != null) {
+					BombSquare neighbourSquare = (BombSquare) board.getSquareAt(nextX, nextY);
+					if (!neighbourSquare.revealed) {
+						neighbourSquare.clicked(); // recursively call clicked() to process neighbouring squares
+					}
 				}
-			}
-		}
-	}
-	
+            }
+        }
+    }
 }
